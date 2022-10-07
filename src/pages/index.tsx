253079f2fd1fe7742eef5bdce71a2a1f90d0fa6d
Abdallah Abedraba <aabedraba@gmail.com>
2022-10-07 09:07:38 +0400

    change to next app only without express

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
+import { GetServerSideProps } from "next";
 import { useState } from "react";
 
-const Index = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+type IndexPageProps = {
+  userLoggedIn: boolean;
+};
+
+const Index = ({ userLoggedIn }: IndexPageProps) => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(userLoggedIn);
+
   return (
     <div>
       {loggedIn ? <p>You're already logged in</p> : <button>Logged in</button>}
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async (
+  context
+) => {
+  return {
+    props: {
+      userLoggedIn: false,
+    },
+  };
 };
 
 export default Index;
